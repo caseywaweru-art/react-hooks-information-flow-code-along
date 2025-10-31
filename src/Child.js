0101fda99e90d33a8d350a0006b16cc2c59eca20
Casey Waweru <casey.waweru@gmail.com>
2025-10-31 05:38:25 +0300

    Forgotten changes

--- src/Child.js
+++ src/Child.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
-function Child({ onChangeColor }) {
-  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+function Child({ color, onChangeColor }) {
+  const newRandomColor = getRandomColor();
+
+  return <div 
+    onClick={() => onChangeColor(newRandomColor)}
+    className="child" 
+    style={{ backgroundColor: color }} 
+  />;
 }
 
 export default Child;
